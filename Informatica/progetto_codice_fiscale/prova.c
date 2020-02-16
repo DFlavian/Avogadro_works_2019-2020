7bfe503076ba41e8b3fffd2ee24415e09a974940
Lepsoy23 <56250240+Lepsoy23@users.noreply.github.com>
2020-02-16 22:44:14 +0100

    Upload css cestini

--- Informatica/progetto_codice_fiscale/prova.c
+++ Informatica/progetto_codice_fiscale/prova.c
@@ -1,8 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "funzioni.h"
+const int dim = 30;
+
 
 int main(){
+    char nome[dim];
+
 	stampa_titolo();
     stampa_menu();
+    gotoxy(50,3);
+    gets(nome);
+    upper_string(nome);
+    gotoxy(50,3);
+    Stampa_vettore(nome);
 }
